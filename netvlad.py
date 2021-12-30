a6b4973bc287fcb84aa28abab1bea37fae5b0b95
jsz0913 <66623990+jsz0913@users.noreply.github.com>
2021-12-30 16:20:03 +0800

    Update netvlad.py

--- netvlad.py
+++ netvlad.py
@@ -28,14 +28,15 @@ class NetVLAD(nn.Module):
         
         # 1 * 1的卷积层相当于全连接
         #（batch,channel,height,width）
-        # H * W 是 特征数目
-        # channel 是 特征
+        # 把通道当特征维 把H*W当样本数
         self.conv = nn.Conv2d(dim, K, kernel_size=(1, 1), bias=True)
+        # 特征中心同样优化
         self.centroids = nn.Parameter(torch.rand(K, dim))
         self._init_params()
 
     def _init_params(self):
-        # cin cout kw kh
+        # W * x + b
+        # 
         self.conv.weight = nn.Parameter(
             (2.0 * self.alpha * self.centroids).unsqueeze(-1).unsqueeze(-1)
         )
@@ -52,24 +53,42 @@ class NetVLAD(nn.Module):
         if self.normalize_input:
             x = F.normalize(x, p = 2, dim = 1)  # across descriptor dim
 
-        # soft-assignment
-
-        soft_assign = self.conv(x).view(B, self.num_clusters, -1)
+        # soft-assignment，对每页上的K作为一个整体
+        soft_assign = self.conv(x).view(B, self.K, -1)
+        # B * K * N
         soft_assign = F.softmax(soft_assign, dim = 1)
-        
-
+        # B * D * N
         x_flatten = x.view(B, D, -1)
         
-       
-        residual = x_flatten.expand(self.num_clusters, -1, -1, -1).permute(1, 0, 2, 3) - \
+        # expand 
+        #   新增加的维度将附在前面 扩大张量不需要分配新内存仅仅是新建一个张量的视图
+        #   任意一个一维张量在不分配新内存情况下都可以扩展为任意的维度。
+        #   传入 -1 则意味着维度扩大不涉及这个维度。
+        # permute 调换顺序，相当于索引过去
+        # 结合起来，就应该是直接看最后结果
+        # H * W -> 3 * H * W
+        # 那么 0 1 2 得到的都是H*W的第一个值
+        # 交换位置后 H * W * 3
+        # 最后一个位置才行
+        
+        # calculate residuals to each clusters
+        # 每个 D 维 找到其对应第 K 个 D 维 的权重 在 K 维度中 
+        # a: (B, D, N)  -> (B, K, D, N)
+        # b: (K , D)  -> (K, D, N)    
+        # residual: (B, K, D, N) 
+        # K * D 代表 一个 D维度 描述子 和 所有中心的 差值 
+        residual = x_flatten.expand(self.K, -1, -1, -1).permute(1, 0, 2, 3) - \
             self.centroids.expand(x_flatten.size(-1), -1, -1).permute(1, 2, 0).unsqueeze(0)
+        # soft_assign: B * K * 1 *  N
         residual *= soft_assign.unsqueeze(2)
       
-
+        # 所有样本加起来
+        # B * K * D
         vlad = residual.sum(dim = -1)  
-
+        # 针对描述子维度
         vlad = F.normalize(vlad, p = 2, dim = 2)  # intra-normalization 
-        vlad = vlad.view(x.size(0), -1)  # flatten   # flatten vald: 
+        # B * (K*D)
+        vlad = vlad.view(x.size(0), -1)  # flatten  
         vlad = F.normalize(vlad, p = 2, dim = 1)  # L2 normalize
         return vlad
 
@@ -82,7 +101,6 @@ class EmbedNet(nn.Module):
 
     def forward(self, x):
         x = self.base_model(x)
-        
         embedded_x = self.net_vlad(x)
         return embedded_x
 
